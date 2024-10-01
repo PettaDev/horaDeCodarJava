224601d0f46a5cd8668d0394457940aca1a34099
Gustavo Petta <160749630+PettaDev@users.noreply.github.com>
2024-10-01 20:12:02 -0300

    Delete src/Ex008.java

--- src/Ex008.java
+++
@@ -1,9 +0,0 @@
-//Faça um programa para ler 2 valores informados pelo usuário
-// e se o segundo valor informado for neutro, deve ser lido um novo valor
-// O programa deve imprimir o resultado da divisão do primeiro valor lido pelo segundo valor lido.
-
-public class Ex008 {
-    public static void main(String[] args) {
-
-    }
-}
